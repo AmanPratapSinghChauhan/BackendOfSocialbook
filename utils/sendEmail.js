62b0c4bb63dfec0275c04dadb7d5bed92f4d936a
Aman Pratap Singh Chauhan <aman0107chauhan2003@gmail.com>
2026-01-05 15:56:35 +0530

    Change Mail conf.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,11 +6,14 @@ export const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
     host: process.env.SMPT_HOST,
     port: process.env.SMPT_PORT,
-    service: process.env.SMPT_SERVICE,
+    secure: false,
     auth: {
       user: process.env.SMPT_MAIL,
       pass: process.env.SMPT_PASSWORD,
     },
+    tls:{
+      rejectUnauthorized: false
+    }
   });
 
   const mailOptions = {
