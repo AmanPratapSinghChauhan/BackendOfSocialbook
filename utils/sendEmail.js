f904bca4d581a88a81ae7dd7edf355c31e481df9
Aman Pratap Singh Chauhan <aman0107chauhan2003@gmail.com>
2026-01-05 16:04:08 +0530

    mail issues

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,27 +1,34 @@
 import nodeMailer from "nodemailer";
 import dotenv from 'dotenv';
-dotenv.config({path:'./Config/config.env'});
+dotenv.config({ path: './Config/config.env' });
 
 export const sendEmail = async (options) => {
-  const transporter = nodeMailer.createTransport({
-    host: process.env.SMPT_HOST,
-    port: process.env.SMPT_PORT,
-    secure: false,
-    auth: {
-      user: process.env.SMPT_MAIL,
-      pass: process.env.SMPT_PASSWORD,
-    },
-    tls:{
-      rejectUnauthorized: false
-    }
-  });
+  try {
+    const transporter = nodeMailer.createTransport({
+      host: process.env.SMPT_HOST,
+      port: process.env.SMPT_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMPT_MAIL,
+        pass: process.env.SMPT_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+    await transporter.verify();
+    console.log("SMTP VERIFIED");
+    const mailOptions = {
+      from: process.env.SMPT_MAIL,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
 
-  const mailOptions = {
-    from: process.env.SMPT_MAIL,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+    await transporter.sendMail(mailOptions);
+    console.log("MAIL SENT");
 
-  await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("MAIL ERROR 👉", error);
+  }
 };
